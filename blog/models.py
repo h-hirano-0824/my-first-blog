747bb2cd60066f5a791469341e665df08980312f
h-hirano-0824 <hirano@cool.email.ne.jp>
2023-06-13 21:22:19 +0900

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -5,13 +5,13 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
-    auther = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
     
-    def published(self):
+    def publish(self):
         self.published_date = timezone.now()
         self.save()
     
